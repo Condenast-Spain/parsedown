2417dbfca22ef45e1c9186ac1be0882e20eb2d20
José María Clemente <jmclemente@condenast.es>
2018-06-12 12:40:46 +0200

    Update.

--- Parsedown.php
+++ Parsedown.php
@@ -5,9 +5,6 @@
 # Parsedown
 # http://parsedown.org
 #
-# (c) Emanuil Rusev
-# http://erusev.com
-#
 # For the full license information, view the LICENSE file that was distributed
 # with this source code.
 #
